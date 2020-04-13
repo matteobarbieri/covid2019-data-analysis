50602fec8e1cc5b2467bf935d987d842b1a8c4ae
Matteo Barbieri <neopard@gmail.com>
2020-04-13 10:19:42 +0200

    Fix bug in updating submodules

--- daily_update.sh
+++ daily_update.sh
@@ -3,8 +3,11 @@
 # Allow the script to exit on error
 set -e
 
+# Pull potential changes in remote repo
+git pull
+
 # Pull new data from remote repos
-git pull --recurse-submodules
+git submodule foreach --recursive 'git pull'
 
 # Activate anaconda environment
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -19,8 +22,8 @@ python ./projections_covid2019_italy.py || \
 # Change the date in README.md
 sed -i "s/^**Updated.*/**Updated $(date +%d\\/%m\\/%Y -d yesterday)**/" README.md
 
-# Stage plots and README.md
-git add plots README.md
+# Stage submodules, plots and README.md
+git add data-ita data-world plots README.md
 
 # Commit
 git commit -m "Daily udpate ($(date +%d/%m/%Y -d yesterday))" || exit 1
